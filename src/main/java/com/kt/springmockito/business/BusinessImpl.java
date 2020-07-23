b9cdfa9b3a058c9eb3720b5ac42423a994dbbcf8
karnakar <karnakar@ymail.com>
2020-07-23 01:39:31 -0400

    Stub unit

--- src/main/java/com/kt/springmockito/business/BusinessImpl.java
+++ src/main/java/com/kt/springmockito/business/BusinessImpl.java
@@ -1,6 +1,10 @@
 package com.kt.springmockito.business;
 
+import com.kt.springmockito.data.BusinessService;
+
 public class BusinessImpl {
+	private BusinessService businessService;
+	
 	public int calculdateSum(int[] nums) {
 		int sum=0;
 		for(int a:nums) {
@@ -9,4 +13,20 @@ public class BusinessImpl {
 		return sum;
 		
 	}
+	
+	public int calculdateSumDataService() {
+		int sum=0;
+		int[] nums=businessService.retrieveAllData();
+		for(int a:nums) {
+			sum+=a;
+		}
+		return sum;
+		
+	}
+
+	public void setBusinessService(BusinessService businessService) {
+		this.businessService = businessService;
+	}
+	
+	
 }
