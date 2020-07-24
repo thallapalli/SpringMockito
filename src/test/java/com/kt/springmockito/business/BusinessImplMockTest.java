ae26433c0635b973ec8ed8af1c2796c1b32b5636
karnakar <karnakar@ymail.com>
2020-07-23 23:29:44 -0400

    Verify added

--- src/test/java/com/kt/springmockito/business/BusinessImplMockTest.java
+++ src/test/java/com/kt/springmockito/business/BusinessImplMockTest.java
@@ -10,6 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -17,21 +19,35 @@ class BusinessImplMockTest {
 	
 	List list=mock(List.class);
 	@Test
-	public void TestSize() {
+	public void testSize() {
 		when(list.size()).thenReturn(5);
 		assertEquals(5,list.size());
 		
 	}
 	@Test
-	public void TestParameter() {
+	public void testParameter() {
 		when(list.get(2)).thenReturn("Karnakar");
 		assertEquals("Karnakar",list.get(2));
 		
 	}
 	@Test
-	public void TestParameter1() {
+	public void testParameterArgumentmatcher() {
 		when(list.get(anyInt())).thenReturn("Karnakar");
 		assertEquals("Karnakar",list.get(3));
 		
 	}
+	@Test
+	public void testParameterArgumentmatcher1() {
+		when(list.get(anyInt())).thenReturn("Karnakar");
+		assertEquals("Karnakar",list.get(3));
+		
+	}
+	@Test
+	public void testVerificationbasics() {
+		String val=(String) list.get(2);
+		String val1=(String) list.get(2);
+		verify(list,times(2)).get(anyInt());
+		
+	}
+	
 }
