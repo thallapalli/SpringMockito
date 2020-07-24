915915bbe7d7a8b38737f899f5df614596f4e76d
karnakar <karnakar@ymail.com>
2020-07-23 22:40:57 -0400

    Mockito Test

--- src/test/java/com/kt/springmockito/business/BusinessImplMockTest.java
+++ src/test/java/com/kt/springmockito/business/BusinessImplMockTest.java
@@ -1,22 +1,25 @@
 package com.kt.springmockito.business;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.kt.springmockito.data.BusinessService;
 
+@ExtendWith(MockitoExtension.class)
 class BusinessImplMockTest {
-	BusinessImpl businessImplTest = new BusinessImpl();
-	BusinessService mock = mock(BusinessService.class);
+	@Mock
+	BusinessService mock;
+	@InjectMocks
+	BusinessImpl businessImplTest ;
+	
 
-	@BeforeEach
-	public void before() {
-		businessImplTest.setBusinessService(mock);
-	}
+	
 
 	@Test
 	void testcalculdateSumDataService() {
